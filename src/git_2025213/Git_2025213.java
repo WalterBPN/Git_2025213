47948e185fe5aa350fb8d920f7b1872fc850aa57
Walter Barcellos Pereira Neto <wbpnok@gmail.com>
2025-05-05 23:44:36 +0100

    empty!

---
+++ src/git_2025213/Git_2025213.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package git_2025213;
+
+/**
+ *
+ * @author Walter
+ */
+public class Git_2025213 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+         System.out.println("Walter - 2025213");
+    }
+    
+}
